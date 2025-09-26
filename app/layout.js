30908c44dc2fefbdc40646be469ef9095fd1a1e5
Manuvrtti <suyash.22b0131169@abes.ac.in>
2025-09-27 05:01:40 +0530

    Vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";  // 👈 import your Provider.jsx (check correct path)
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
