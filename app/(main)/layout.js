f86db23941e6f0f2fc6746fbcdf8c255cca950e6
Manuvrtti <suyash.22b0131169@abes.ac.in>
2025-09-28 02:39:17 +0530

    Merge branch 'master' of https://github.com/manuVrtti/select_iq

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,16 +1,56 @@
-import React from 'react'
-import DashboardProvider from './provider'
+// import React from 'react'
+// import DashboardProvider from './provider'
+
+// function DashboardLayout({ children }) {
+//   return (
+//     <div>
+//         <DashboardProvider>
+//           <div className='p-10'>
+//             {children}
+//           </div>  
+//         </DashboardProvider>
+//     </div>
+//   )
+// }
+
+// export default DashboardLayout
+
+//------new bc----------
+
+"use client";
+import React, { useEffect, useState } from "react";
+import { supabase } from "@/services/supabaseClient";
+import { useRouter } from "next/navigation";
+import DashboardProvider from "./provider";
 
 function DashboardLayout({ children }) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data }) => {
+      if (!data.session) {
+        // 🚫 agar login nahi hai → auth page pe bhej do
+        router.push("/auth");
+      }
+      setLoading(false);
+    });
+  }, [router]);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        Checking authentication...
+      </div>
+    );
+  }
+
   return (
-    <div>
-        <DashboardProvider>
-          <div className='p-10'>
-            {children}
-          </div>  
-        </DashboardProvider>
-    </div>
-  )
+    <DashboardProvider>
+      <div className="p-10">{children}</div>
+    </DashboardProvider>
+  );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
+
